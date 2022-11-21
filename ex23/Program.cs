443f3bc86581459d4f64e188a42871130b42d823
angela <nik1955mix@gmail.com>
2022-11-22 02:12:41 +0300

    ex23

---
+++ ex23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23/ Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите любое число: ");  
+int a = 0;
+a = Convert.ToInt32(Console.ReadLine());
+
+string CubeTable(int a)
+{
+    int count = 1;
+    string table = "";
+    while (count <= a)
+    {
+        table = table + $" {count} -> | {count * count * count} \a,";
+        count++;
+    }
+    return table;
+}
+string cubeTable = CubeTable(a);
+Console.Write(cubeTable);
+
+
+
+ 
